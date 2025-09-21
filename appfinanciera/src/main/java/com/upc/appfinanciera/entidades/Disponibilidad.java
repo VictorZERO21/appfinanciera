2d5a490c8f0a24982136f074400421f31a6a6843
Diego Alonso Malmaceda Campana <149623076+Raw2406@users.noreply.github.com>
2025-09-21 01:48:34 -0500

    Se creó la entidad Disponibilidad

---
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Disponibilidad.java
@@ -0,0 +1,31 @@
+package com.upc.appfinanciera.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Disponibilidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDisponibilidad;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate fecha; // día disponible
+
+    private boolean disponible;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_asesor", referencedColumnName = "idAsesor")
+    private AsesorFinanciero asesorFinanciero;
+}
