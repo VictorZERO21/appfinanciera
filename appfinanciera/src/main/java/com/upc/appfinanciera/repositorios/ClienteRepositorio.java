1aa2c22c4a1253aa871528e64303afca8e5bd16a
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-09-22 23:16:28 -0500

    Update ClienteRepositorio.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ClienteRepositorio.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ClienteRepositorio.java
@@ -3,6 +3,10 @@ package com.upc.appfinanciera.repositorios;
 import com.upc.appfinanciera.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
     Cliente findByDni(String dni);
+    Cliente findByEmail(String email);
+    List<Cliente> findByNombreContainingIgnoreCase(String nombre);
 }
