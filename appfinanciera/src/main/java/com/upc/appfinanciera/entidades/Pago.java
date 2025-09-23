79f12e47d61dccefa89340f1a32a59ede005afc8
Nicky-1s <tn564642@gmail.com>
2025-09-22 23:27:14 -0500

    Create Pago.java

---
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Pago.java
@@ -0,0 +1,39 @@
+package com.upc.appfinanciera.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReserva;
+
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
+
+    private String estado;
+    private String modalidad;
+
+    @ManyToOne
+    @JoinColumn(name = "idCliente", referencedColumnName = "idCliente")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "idAsesor", referencedColumnName = "idAsesor")
+    private AsesorFinanciero asesor;
+
+    @OneToOne
+    @JoinColumn(name = "idPago")
+    private Pago pago;
+}
