59a9bc825905222d17f24b4374adeef893295a3c
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-09-22 21:04:59 -0500

    Update IAsesorService.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IAsesorService.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IAsesorService.java
@@ -1,12 +1,1 @@
-package com.upc.appfinanciera.interfaces;
 
-import com.upc.appfinanciera.dto.AsesorFinancieroDTO;
-import java.util.List;
-
-public interface IAsesorService {
-    public AsesorFinancieroDTO insertar(AsesorFinancieroDTO asesor);
-    public AsesorFinancieroDTO actualizar(AsesorFinancieroDTO asesor);
-    public void eliminar(String dni);
-    public AsesorFinancieroDTO buscarPorDni(String dni);
-    public List<AsesorFinancieroDTO> buscarTodos();
-}
