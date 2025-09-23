4b6b424aa81451a90cdd1b7f1a4cd8d9f1c6deb2
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-09-22 23:16:09 -0500

    Update AsesorRepositorio.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/AsesorRepositorio.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/AsesorRepositorio.java
@@ -3,6 +3,11 @@ package com.upc.appfinanciera.repositorios;
 import com.upc.appfinanciera.entidades.AsesorFinanciero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AsesorRepositorio extends JpaRepository<AsesorFinanciero, Long> {
     AsesorFinanciero findByDni(String dni);
+    AsesorFinanciero findByEmail(String email);
+    List<AsesorFinanciero> findByEspecialidadContainingIgnoreCase(String especialidad);
+    List<AsesorFinanciero> findByNombreContainingIgnoreCase(String nombre);
 }
