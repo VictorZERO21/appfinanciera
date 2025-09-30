6a01252267375e7c940dfc38679074d8d0b99085
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-09-29 22:39:42 -0500

    Create Pago.java

---
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Pago.java
@@ -0,0 +1,28 @@
+package com.upc.appfinanciera.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pagos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPago;
+
+    private String nombreTarjeta;
+    private String numeroTarjeta;
+
+    private int mesExpiracion;
+    private int anioExpiracion;
+
+    private String cvc;
+    private double montoTotal;
+}
