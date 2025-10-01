6c663b82016ef7180ff952fb9e95ff782315531d
VictorZERO21 <149978504+VictorZERO21@users.noreply.github.com>
2025-10-01 03:43:46 -0500

    Update ChatRepositorio.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ChatRepositorio.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ChatRepositorio.java
@@ -2,7 +2,18 @@ package com.upc.appfinanciera.repositorios;
 
 import com.upc.appfinanciera.entidades.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChatRepositorio extends JpaRepository<Chat, Long> {
+    // Listar todos los mensajes de un chat entre un cliente y un asesor
+    List<Chat> findByCliente_IdClienteAndAsesor_IdAsesor(Long idCliente, Long idAsesor);
 
+    // Listar todos los chats de un asesor (o sea, con todos los clientes)
+    List<Chat> findByAsesor_IdAsesor(Long idAsesor);
+
+    // Si quieres obtener solo los clientes distintos de un asesor:
+    @Query("SELECT DISTINCT c.cliente.idCliente FROM Chat c WHERE c.asesor.idAsesor = :idAsesor")
+    List<Long> findDistinctClientesByAsesor(Long idAsesor);
 }
