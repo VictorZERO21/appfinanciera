c905adbf4121217868535f9c6bc6fab502a58852
Nicky-1s <tn564642@gmail.com>
2025-10-03 01:28:24 -0500

    Update IDisponibilidadService.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IDisponibilidadService.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IDisponibilidadService.java
@@ -5,10 +5,9 @@ import com.upc.appfinanciera.dto.DisponibilidadDTO;
 import java.util.List;
 
 public interface IDisponibilidadService {
-    DisponibilidadDTO insertar(DisponibilidadDTO disponibilidadDto);
+    List<DisponibilidadDTO> insertar(DisponibilidadDTO disponibilidadDto);
     List<DisponibilidadDTO> buscarTodos();
     List<DisponibilidadDTO> buscarPorAsesor(Long idAsesor);
     void eliminar(Long idDisponibilidad);
-
     DisponibilidadDTO actualizar(DisponibilidadDTO disponibilidadDto);
 }
