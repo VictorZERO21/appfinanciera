2559511ddf0c9b9718ee4ceb1a500d19a32e179a
Nicky-1s <tn564642@gmail.com>
2025-10-03 01:15:26 -0500

    Update and rename PagoRepositorio.java to TarjetaRepositorio.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/TarjetaRepositorio.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/TarjetaRepositorio.java
@@ -1,9 +1,9 @@
 package com.upc.appfinanciera.repositorios;
 
-import com.upc.appfinanciera.entidades.Pago;
+import com.upc.appfinanciera.entidades.Tarjeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PagoRepositorio extends JpaRepository<Pago, Long> {
+public interface TarjetaRepositorio extends JpaRepository<Tarjeta, Long> {
 }
