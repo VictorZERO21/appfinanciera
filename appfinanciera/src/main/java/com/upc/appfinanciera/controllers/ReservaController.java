36d306eb84cd847ca8f6cf532e9877c0e07c59c2
Nicky-1s <tn564642@gmail.com>
2025-10-05 18:46:55 -0500

    Update ReservaController.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/controllers/ReservaController.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/controllers/ReservaController.java
@@ -18,19 +18,16 @@ public class ReservaController {
 
    @Autowired
     private IReservaService reservaService;
-
     @PostMapping("/reservas")
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<ReservaDTO> insertar(@Valid @RequestBody ReservaDTO reservaDTO) {
         return new ResponseEntity<>(reservaService.insertarReserva(reservaDTO), HttpStatus.CREATED);
     }
-
     @PutMapping
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<ReservaDTO> modificar(@Valid @RequestBody ReservaDTO reservaDTO) {
         return ResponseEntity.ok(reservaService.modificarReserva(reservaDTO));
     }
-
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public void eliminar(@PathVariable Long id) {
@@ -42,35 +39,19 @@ public class ReservaController {
     public ResponseEntity<ReservaDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(reservaService.buscarReservaPorId(id));
     }
-
     @GetMapping
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<List<ReservaDTO>> listar() {
         return ResponseEntity.ok(reservaService.listarReservas());
     }
-
     @GetMapping("/cliente/{idCliente}")
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<List<ReservaDTO>> listarPorClienteId(@PathVariable Long idCliente) {
         return ResponseEntity.ok(reservaService.listarReservasPorClienteId(idCliente));
     }
-
-    @GetMapping("/cliente/dni/{dniCliente}")
-    @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<List<ReservaDTO>> listarPorClienteDni(@PathVariable String dniCliente) {
-        return ResponseEntity.ok(reservaService.listarReservasPorClienteDni(dniCliente));
-    }
-
     @GetMapping("/asesor/{idAsesor}")
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<List<ReservaDTO>> listarPorAsesorId(@PathVariable Long idAsesor) {
         return ResponseEntity.ok(reservaService.listarReservasPorAsesorId(idAsesor));
     }
-
-    @GetMapping("/asesor/dni/{dniAsesor}")
-    @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<List<ReservaDTO>> listarPorAsesorDni(@PathVariable String dniAsesor) {
-        return ResponseEntity.ok(reservaService.listarReservasPorAsesorDni(dniAsesor));
-    }
 }
-
