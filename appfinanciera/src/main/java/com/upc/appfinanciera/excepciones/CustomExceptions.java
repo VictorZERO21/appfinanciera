f3acd079d7be04be41544d698560b2de0b7c6026
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-10-05 18:41:51 -0500

    Update CustomExceptions.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/excepciones/CustomExceptions.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/excepciones/CustomExceptions.java
@@ -2,44 +2,43 @@ package com.upc.appfinanciera.excepciones;
 
 public class CustomExceptions {
 
-    // Excepción para cuando no se encuentra un Asesor
     public static class AsesorNotFoundException extends RuntimeException {
         public AsesorNotFoundException(String message) {
             super(message);
         }
     }
 
-    // Excepción para cuando no se encuentra un Cliente
     public static class ClienteNotFoundException extends RuntimeException {
         public ClienteNotFoundException(String message) {
             super(message);
         }
     }
 
-    // Excepción para cuando no se encuentra una Reserva
     public static class ReservaNotFoundException extends RuntimeException {
         public ReservaNotFoundException(String message) {
             super(message);
         }
     }
 
-    // Excepción para cuando no se encuentra una Gestión Financiera
+    public static class PagoNotFoundException extends RuntimeException {
+        public PagoNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    public static class DisponibilidadNotFoundException extends RuntimeException {
+        public DisponibilidadNotFoundException(String message) {
+            super(message);
+        }
+    }
+
     public static class GestionFinancieraNotFoundException extends RuntimeException {
         public GestionFinancieraNotFoundException(String message) {
             super(message);
         }
     }
-
-    // Excepción para cuando no se encuentra un Consejo
-    public static class ConsejoNotFoundException extends RuntimeException {
-        public ConsejoNotFoundException(String message) {
-            super(message);
-        }
-    }
-
-    // Excepción para cuando ocurre un error en la Calculadora
-    public static class CalculadoraException extends RuntimeException {
-        public CalculadoraException(String message) {
+    public static class ValidationException extends RuntimeException {
+        public ValidationException(String message) {
             super(message);
         }
     }
