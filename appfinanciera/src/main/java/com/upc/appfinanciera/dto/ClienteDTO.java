fd512d52ab3c2128a3558580b7d3606da04b4175
Nicky-1s <tn564642@gmail.com>
2025-10-05 18:52:14 -0500

    Update ClienteDTO.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/dto/ClienteDTO.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/dto/ClienteDTO.java
@@ -15,23 +15,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ClienteDTO {
     private Long idCliente;
-
     @NotBlank(message = "El DNI no puede estar vacío")
     @Size(min = 8, max = 12, message = "El DNI debe tener entre 8 y 12 caracteres")
     private String dni;
-
     @NotNull(message = "El nombre no puede estar vacío")
     @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
     private String nombre;
     private String password;
-
-
     @Email(message = "El correo electrónico debe ser válido")
     private String email;
     private String sobreMi;
-
-
     @Size(max = 9, message = "El teléfono no debe superar los 9 dígitos")
     private String telefono;
-
 }
