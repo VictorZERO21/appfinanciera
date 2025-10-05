5e4f77faafe0f2ab0b083a844f8dff6e9146ded4
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-10-05 18:44:00 -0500

    Update IChatServicie.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IChatServicie.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IChatServicie.java
@@ -8,10 +8,7 @@ import java.util.List;
 public interface IChatServicie {
     ChatDTO insertar(ChatDTO chatDTO);
     List<ChatDTO> listar();
-    ChatDTO modificar(ChatDTO chatDTO);
-    void eliminar(Long id);
     List<ChatDTO> listarPorClienteYAsesor(Long idCliente, Long idAsesor);
     List<ChatDTO> listarPorAsesor(Long idAsesor);
-    List<Long> listarClientesDeAsesor(Long idAsesor);
 
 }
