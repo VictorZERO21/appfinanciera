1db739516d9508f45c6b3faa0f40471f45270aa8
Fervelsar <137841566+Fervelsar@users.noreply.github.com>
2025-10-05 18:44:17 -0500

    Update IClienteService.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IClienteService.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IClienteService.java
@@ -4,12 +4,8 @@ import com.upc.appfinanciera.dto.ClienteDTO;
 import java.util.List;
 
 public interface IClienteService {
-    ClienteDTO insertarCliente(ClienteDTO clienteDTO);
-    ClienteDTO modificarCliente(ClienteDTO clienteDTO);
-    void eliminarCliente(Long id);
     ClienteDTO buscarClientePorId(Long id);
-    ClienteDTO buscarClientePorDni(String dni);
-    ClienteDTO buscarClientePorEmail(String email);
     List<ClienteDTO> listarClientes();
-    List<ClienteDTO> buscarClientesPorNombre(String nombre);
 }
+
+
