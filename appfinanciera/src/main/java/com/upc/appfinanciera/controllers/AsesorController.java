e67354867e724b80316e720f7e9ec4542f5ba20c
Nicky-1s <tn564642@gmail.com>
2025-10-05 18:42:52 -0500

    Update AsesorController.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/controllers/AsesorController.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/controllers/AsesorController.java
@@ -1,6 +1,7 @@
 package com.upc.appfinanciera.controllers;
 
 import com.upc.appfinanciera.dto.AsesorFinancieroDTO;
+import com.upc.appfinanciera.dto.ClienteDTO;
 import com.upc.appfinanciera.interfaces.IAsesorService;
 import com.upc.appfinanciera.servicios.AsesorService;
 import jakarta.validation.Valid;
@@ -19,54 +20,14 @@ public class AsesorController {
     @Autowired
     private IAsesorService asesorService;
 
-   // @PostMapping
-   // public ResponseEntity<AsesorFinancieroDTO> insertar(@Valid @RequestBody AsesorFinancieroDTO asesorDTO) {
-   //     return ResponseEntity.ok(asesorService.insertarAsesor(asesorDTO));
-   // }
-
-   // @PutMapping
-   // public ResponseEntity<AsesorFinancieroDTO> modificar(@Valid @RequestBody AsesorFinancieroDTO asesorDTO) {
-   //     return ResponseEntity.ok(asesorService.modificarAsesor(asesorDTO));
-   // }
-
-   //@DeleteMapping("/{id}")
-   //public void eliminar(@PathVariable Long id) {
-   //    asesorService.eliminarAsesor(id);
-   //}
-
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<AsesorFinancieroDTO> buscarPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(asesorService.buscarAsesorPorId(id));
-    }
-
-    @GetMapping("/dni/{dni}")
-    @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<AsesorFinancieroDTO> buscarPorDni(@PathVariable String dni) {
-        return ResponseEntity.ok(asesorService.buscarAsesorPorDni(dni));
-    }
-
-    @GetMapping("/email/{email}")
-    @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<AsesorFinancieroDTO> buscarPorEmail(@PathVariable String email) {
-        return ResponseEntity.ok(asesorService.buscarAsesorPorEmail(email));
-    }
-
     @GetMapping
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
     public ResponseEntity<List<AsesorFinancieroDTO>> listar() {
         return ResponseEntity.ok(asesorService.listarAsesores());
     }
-
-    @GetMapping("/nombre/{nombre}")
+    @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('CLIENTE','ASESOR')")
-    public ResponseEntity<List<AsesorFinancieroDTO>> buscarPorNombre(@PathVariable String nombre) {
-        return ResponseEntity.ok(asesorService.buscarAsesoresPorNombre(nombre));
+    public ResponseEntity<AsesorFinancieroDTO> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(asesorService.buscarAsesorPorId(id));
     }
-
-  // @GetMapping("/especialidad/{especialidad}")
-  // public ResponseEntity<List<AsesorFinancieroDTO>> buscarPorEspecialidad(@PathVariable String especialidad) {
-  //     return ResponseEntity.ok(asesorService.buscarAsesoresPorEspecialidad(especialidad));
-  // }
 }
-
