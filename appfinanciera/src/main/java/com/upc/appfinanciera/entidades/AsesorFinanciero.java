28d9b5ef1ae8ed2554c4a1057604c948465111b6
Nicky-1s <tn564642@gmail.com>
2025-10-05 18:59:35 -0500

    Update AsesorFinanciero.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/entidades/AsesorFinanciero.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/AsesorFinanciero.java
@@ -12,28 +12,19 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AsesorFinanciero {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAsesor;
-
     @Column(unique = true, nullable = false, length = 20)
     private String dni;
-
     @Column(length = 100, nullable = false)
     private String nombre;
-
     private String telefono;
-
     private String password;
-
     private String email;
     private String sobreMi;
-
-
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private Perfil user;
 
 }
-
