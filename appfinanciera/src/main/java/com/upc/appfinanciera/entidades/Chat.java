6e0079043c8c1b363f68fa5ae57bbcc6dec14e22
Nicky-1s <tn564642@gmail.com>
2025-10-05 19:26:11 -0500

    Update Chat.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Chat.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Chat.java
@@ -18,21 +18,16 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idChat;
-
     @ManyToOne
     @JoinColumn(name = "idCliente", nullable = false)
     private Cliente cliente;
-
     @ManyToOne
     @JoinColumn(name = "idAsesor", nullable = false)
     private AsesorFinanciero asesor;
-
     @Column(nullable = false, length = 750)
     private String comentario;
-
     @Column(nullable = false)
     private LocalDateTime fechaHora;
-
     @PrePersist
     public void asignarFechaHora()
     {
