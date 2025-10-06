6f39ae054ee3f50d845146affd4f9c70837356f5
Nicky-1s <tn564642@gmail.com>
2025-10-05 19:26:59 -0500

    Update Disponibilidad.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Disponibilidad.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Disponibilidad.java
@@ -19,18 +19,13 @@ public class Disponibilidad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDisponibilidad;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate fecha;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime horaInicio;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime horaFin;
-
     private boolean disponible;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_asesor", referencedColumnName = "idAsesor")
     private AsesorFinanciero asesorFinanciero;
