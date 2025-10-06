a72b95469f4b513d7151b7176ffcd0d7409a8816
Nicky-1s <tn564642@gmail.com>
2025-10-05 19:27:18 -0500

    Update GestionFinanciera.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/entidades/GestionFinanciera.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/GestionFinanciera.java
@@ -17,17 +17,11 @@ public class GestionFinanciera {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idGestion;
-
     private String titulo;
-
     private String tipo;
-
     private double monto;
-
     private LocalDate fecha;
-
-    // Relación con Cliente usando la entidad Cliente completa
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_cliente", referencedColumnName = "idCliente")  // Asegúrate de usar la columna idCliente
-    private Cliente cliente;  // Relación con Cliente completo
+    @JoinColumn(name = "id_cliente", referencedColumnName = "idCliente")
+    private Cliente cliente;
 }
