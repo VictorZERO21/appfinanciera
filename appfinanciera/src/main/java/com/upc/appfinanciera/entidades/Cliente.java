ef6b19d9da07f2cc6a313009f10c15d4aa85294a
Nicky-1s <tn564642@gmail.com>
2025-10-05 19:26:42 -0500

    Update Cliente.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Cliente.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/entidades/Cliente.java
@@ -15,23 +15,16 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCliente;
-
     @Column(unique = true, nullable = false, length = 20)
     private String dni;
-
     @Column(length = 100, nullable = false)
     private String nombre;
     private String password;
-
     private String email;
-
     private String telefono;
     private String sobreMi;
-
-
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private Perfil user;
 
 }
-
