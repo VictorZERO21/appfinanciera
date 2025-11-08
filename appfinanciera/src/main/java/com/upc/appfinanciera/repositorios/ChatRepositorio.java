4c9ac647432fc8c2a73c45710050a454102e1498
Nicky-1s <tn564642@gmail.com>
2025-11-08 00:12:46 -0500

    Update ChatRepositorio.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ChatRepositorio.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/repositorios/ChatRepositorio.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ChatRepositorio extends JpaRepository<Chat, Long> {
     List<Chat> findByCliente_IdClienteAndAsesor_IdAsesor(Long idCliente, Long idAsesor);
     List<Chat> findByAsesor_IdAsesor(Long idAsesor);
+    List<Chat> findByCliente_IdCliente(Long idCliente);
+
 }
