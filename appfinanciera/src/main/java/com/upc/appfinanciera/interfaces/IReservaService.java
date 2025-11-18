4c6f9d5b25a5871619acb853dffa78ca0018c636
Nicky-1s <tn564642@gmail.com>
2025-11-18 08:03:23 -0500

    Update IReservaService.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IReservaService.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IReservaService.java
@@ -1,6 +1,8 @@
 package com.upc.appfinanciera.interfaces;
 
 import com.upc.appfinanciera.dto.ReservaDTO;
+import com.upc.appfinanciera.dto.ClienteDTO;
+
 import java.util.List;
 
 public interface IReservaService {
@@ -13,4 +15,5 @@ public interface IReservaService {
     List<ReservaDTO> listarReservasPorAsesorId(Long idAsesor);
     List<ClienteDTO> listarClientesConReservas();
 }
+
 
