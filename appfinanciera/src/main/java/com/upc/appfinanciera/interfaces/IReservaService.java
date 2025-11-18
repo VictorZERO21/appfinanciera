6f94102d4a345a77463131601314991d71e12126
Nicky-1s <tn564642@gmail.com>
2025-11-18 07:56:09 -0500

    Update IReservaService.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IReservaService.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/interfaces/IReservaService.java
@@ -11,4 +11,6 @@ public interface IReservaService {
     List<ReservaDTO> listarReservas();
     List<ReservaDTO> listarReservasPorClienteId(Long idCliente);
     List<ReservaDTO> listarReservasPorAsesorId(Long idAsesor);
+    List<ClienteDTO> listarClientesConReservas();
 }
+
