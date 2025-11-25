ee8291d33b91ea4213ecc3373e15018dff923419
esteph <143745653+estepss@users.noreply.github.com>
2025-11-24 22:59:18 -0500

    Update GlobalExceptionHandler.java

--- appfinanciera/src/main/java/com/upc/appfinanciera/excepciones/GlobalExceptionHandler.java
+++ appfinanciera/src/main/java/com/upc/appfinanciera/excepciones/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.upc.appfinanciera.excepciones;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,7 +33,43 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleValidation(CustomExceptions.ValidationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
 
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String campo = error.getField();            // ej: "dni"
+            String mensaje = error.getDefaultMessage(); // ej: "El DNI debe tener 8 dígitos."
+            errores.put(campo, mensaje);
+        });
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Error de validación");
+        body.put("type", "error");
+        body.put("errors", errores);  //van  mensajes del DTO
+
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDatabaseConstraint(DataIntegrityViolationException ex) {
+        String cause = ex.getMostSpecificCause().getMessage().toLowerCase();
+        String mensaje = "Dato duplicado.";
+        if (cause.contains("dni")) {
+            mensaje = "El DNI ya está registrado.";
+        } else if (cause.contains("cliente_email_key") || cause.contains("clien_email")) {
+            mensaje = "El correo ya está registrado.";
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                Map.of(
+                        "message", mensaje,
+                        "type", "error",
+                        "code", "DUPLICADO"
+                )
+        );
+    }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneric(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
